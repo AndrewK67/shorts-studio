48fcfc6bf1fa3698f74e1365ee78435be3a6aa29
AndrewSK67 <51203916+AndrewSK67@users.noreply.github.com>
2025-11-18 08:34:27 +0000

    Manual fix for login page and config

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -44,7 +44,7 @@ export default function LoginPage() {
           appearance={{ theme: ThemeSupa }}
           providers={['google', 'github']}
           theme="light"
-          redirectTo={${process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'}/auth/callback}
+          redirectTo={`${process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'}/auth/callback`}
         />
       </div>
     </div>
